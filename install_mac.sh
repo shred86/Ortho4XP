4092fbeec8923a43679f1884a1aa2fe9aefbcd04
shred86 <shred86@icloud.com>
2024-05-30 15:54:53 -0700

    update macOS instructions

--- install_mac.sh
+++
@@ -1,8 +0,0 @@
-# Install dependencies
-brew install python gdal spatialindex p7zip proj python-tk
-
-# Install pyproj
-pip3 install pyproj
-
-# Install other dependencies
-pip3 install numpy shapely rtree pillow requests
